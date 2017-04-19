31686063e937642d775fb3178b2e4cbc85d337cb
Yinxia Zhang <274581843@qq.com>
2017-04-19 13:57:03 +0800

    'EX05'

---
+++ js/EX05(01).js
@@ -0,0 +1,39 @@
+/*1*/
+$('.ui-cnt-img').click(function(event) {
+	/* Act on the event */
+	var src=$(this).attr('src');
+	$('.ui-hidden-img').attr('src',src);
+	$('.ui-cnt').css({'filter':'blur(10px)'});
+	$('.ui-hidden').fadeIn('fast', function() {
+		$(this).click(function(event) {
+			/* Act on the event */
+			$(this).fadeOut('slow/400/fast');
+			$('.ui-cnt').css({'filter':'blur(0px)'});
+		});
+	});
+});
+/*2*/
+$('.row2ui-title-cnt').click(function(event) {
+	/* Act on the event */
+	$('.row2ui-title-cnt').css({'background-color':'white'});
+	$(this).css({'background-color':'lightgray'});
+	var ind=$('.row2ui-title-cnt').index(this);
+	//alert(ind);
+	$('.row2ui-main-cnt').text(ind+1);
+});
+/*3*/
+$('.row3ui-main-right').on('click',remove);
+
+function remove(){
+	$(this).parent().remove();
+	for(var i=0;i<$('.row3ui-main-left').length;i++)
+		$('.row3ui-main-left').eq(i).html(i+1);
+}
+$('.row3ui-button').click(function(event) {
+	/* Act on the event */
+	var i=$('.row3ui-main').length;
+	i++;
+	var node="<div class='row3ui-main'><div class='row3ui-main-left'>"+i+"</div><div class='row3ui-main-right'>delete</div></div>";
+	$('.row3ui-button').before(node);
+	$('.row3ui-main-right:last').on('click',remove);
+});
